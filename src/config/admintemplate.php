93fc7b1d6b949648eef9706ef8d0a9b64f874555
kamrul <kamruljpi@gmail.com>
2020-02-02 17:04:23 +0600

    first initial commit

--- src/config/admintemplate.php
+++ src/config/admintemplate.php
@@ -7,7 +7,7 @@ return [
 
         'package_assets' => base_path('vendor/kamruljpi/admintemplate/src/assets'),
 
-        'base_assets' => base_path(),
+        'base_assets' => base_path()."/../",
 
         'base_views' => base_path('resources/views/kamruljpi/admintemplate'),
 
